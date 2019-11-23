da7cf29ea5927c97dd89e59df5e73606b0554a52
Zeyuan (Max) Niu <nium3720@36wolverine.cs.uleth.ca>
2019-11-23 15:23:03 -0700

    here is the code

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/**
+ * @author max niu , matthew wilbern
+ * @date Nov 22, 2019
+ * @copyright 2019
+ */
+
+#include <iostream>
+#include <fstream>
+#include <random>
+
+using namespace std;
+int main (){
+   int num;
+
+   ofstream myfile;
+
+
+   cout<<"enter the number of points"<<endl;
+   cin>>num;
+
+   myfile.open("output100000.txt");
+    random_device x;
+    random_device y;
+    uniform_int_distribution<int> dist(-10000,10000);
+   for(int i = 0;i<num;i++) {
+        cout<<dist(x)<<"   "<<dist(y)<<endl;
+
+  myfile<<dist(x)<<"   "<<dist(y)<<endl;
+
+
+
+}
+myfile.close();
+return 0;
+
+}
+
