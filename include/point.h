07bc2ba4b468819497a2517bef6346839aef6288
Matthew Wilbern <matthew.wilbern@uleth.ca>
2019-11-23 17:43:20 -0700

    added comparisons

--- include/point.h
+++ include/point.h
@@ -1,7 +1,14 @@
 #ifndef POINT_H
 
 #define POINT_H
-struct point {
+class point {
+ public:
+  static bool compareX(p1, p2) {
+    return p1.x < p2.x;
+  }
+  static bool compareY(p1, p2) {
+    return p1.y < p2.y;
+  }
   int x;
   int y;
 };
