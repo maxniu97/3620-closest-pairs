2c96d0335d8fe3e19c97ed3ad7d6c6968ceaa47a
Matthew Wilbern <matthew.wilbern@uleth.ca>
2019-11-24 17:21:02 -0700

    i unno

--- bruteforce.cpp
+++ bruteforce.cpp
@@ -22,24 +22,13 @@ double dist(point p1, point p2) {
 
 double bruteForce(vector<point> p) {
   double dmin = 10000;
-  point p1, p2;
   int n = p.size();
 
   for(int i = 0; i< n; i++)
     for (int j =i+1; j< n; j++)
       if(dist(p.at(i),p.at(j))<dmin) {
         dmin = dist(p.at(i),p.at(j));
-        p1 = p.at(i);
-        p2 = p.at(j);
       }
-
-  cout << "Closest points at: \n"
-       << "1:\n"
-       << "  x: " << p1.x << endl
-       << "  y: " << p1.y << endl
-       << "2:\n"
-       << "  x: " << p2.x << endl
-       << "  y: " << p2.y << endl;
 
   return dmin;
 }
